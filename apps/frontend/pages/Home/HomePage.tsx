36a7fb2f2b78f4402392e423f033b254583aef73
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-08-29 15:57:54 +0100

    Building hero page

--- apps/frontend/pages/Home/HomePage.tsx
+++ apps/frontend/pages/Home/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 const HomePage = () => {
    return (
-      <div>
+      <div className="flex flex-col items-center">
          <Header />
+         <Hero />
       </div>
    )
 }
---
+++ apps/frontend/pages/Home/components/Hero.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Hero = () => {
+   return (
+      <div className="border w-[60em] flex">
+         <div className =" headline border w-[40em] h-[20em]">
+            <img />
+
+            <h2>Headline </h2>
+            <p>Something about the headline</p>
+
+         </div>
+         {/* more news  */}
+         <div className="border w-[20em]">
+            <div className="border h-[5em]">
+
+            </div>
+            <div className="border h-[5em]"> 
+
+            </div>
+            <div className="border h-[5em]">
+
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default Hero
